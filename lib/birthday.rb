d15ff5fced0894332f42e4cb02f717c639bc1b24
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-02 19:15:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 def age_appropriate_birthday(kids)
   kids.each do |name, age|
     if age < 12
-      puts "Happy Birthday #{name}! You are now #{age} years old!"
+      print "Happy Birthday #{name}! You are now #{age} years old!"
     end
   end
 end
