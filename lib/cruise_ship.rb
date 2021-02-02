e798d00c04382e76b660c4a1eac9acd5e57504d1
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-02 19:11:48 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,8 +9,8 @@
 def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name
+      return winner = name
     end
   end
-  winner
+
 end
